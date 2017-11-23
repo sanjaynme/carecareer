f1665068443dfa07a84a20c84d24288f89e06cc3
sanjul910bm <sanju.l910bm@gmail.com>
2017-11-23 17:26:00 +0545

    added botto, naviagtion view

--- app/src/main/java/au/com/carecareers/android/homeModule/HomeActivity.java
+++ app/src/main/java/au/com/carecareers/android/homeModule/HomeActivity.java
@@ -3,7 +3,9 @@ package au.com.carecareers.android.homeModule;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -19,17 +21,18 @@ import butterknife.BindView;
  * Created by Nikesh on 11/20/2017.
  */
 
-public class HomeActivity extends BaseActivity implements HomeContract.IHomeView {
+public class HomeActivity extends BaseActivity implements HomeContract.IHomeView, BottomNavigationView.OnNavigationItemSelectedListener {
     @BindView(R.id.home_toolbar)
     Toolbar toolbar;
 
     @BindView(R.id.tv_toolbar_title)
     TextView tvTitle;
 
- /*   @BindView(R.id.bottom_nav_bar)
+    @BindView(R.id.bottom_nav_bar)
     BottomNavigationView bottomNavigationView;
-    */
+
     private LoginModel.LoginRespones loginResponses;
+    private static int currentTabId;
 
     public static void start(Context context) {
         Intent homeIntent = new Intent();
@@ -51,6 +54,8 @@ public class HomeActivity extends BaseActivity implements HomeContract.IHomeView
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        currentTabId = bottomNavigationView.getSelectedItemId();
     }
 
     @Override
@@ -64,11 +69,47 @@ public class HomeActivity extends BaseActivity implements HomeContract.IHomeView
         return true;
     }
 
-
     @Override
     public void setupToolbar() {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         tvTitle.setText(getResources().getText(R.string.tv_home));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (getSupportFragmentManager().getBackStackEntryCount() == 0 && currentTabId == item.getItemId()) {
+
+        } else {
+            switch (item.getItemId()) {
+                case R.id.menu_search:
+                    currentTabId = item.getItemId();
+                    break;
+
+                case R.id.menu_saved_lists:
+                    if (getSupportFragmentManager().getBackStackEntryCount() > 1 || currentTabId != item.getItemId()) {
+                        currentTabId = item.getItemId();
+                    }
+                    break;
+
+                case R.id.menu_applied_jobs:
+                    if (getSupportFragmentManager().getBackStackEntryCount() > 1 || currentTabId != item.getItemId()) {
+                        currentTabId = item.getItemId();
+                    }
+                    break;
+
+                case R.id.menu_job_alerts:
+                    if (getSupportFragmentManager().getBackStackEntryCount() > 1 || currentTabId != item.getItemId()) {
+                        currentTabId = item.getItemId();
+                    }
+                    break;
+                case R.id.menu_profile:
+                    if (getSupportFragmentManager().getBackStackEntryCount() > 1 || currentTabId != item.getItemId()) {
+                        currentTabId = item.getItemId();
+                    }
+                    break;
+            }
+        }
+        return true;
     }
 }
