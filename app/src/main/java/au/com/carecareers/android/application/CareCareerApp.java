26fca08d73ffb443df66f3c74ab753420e21b1cb
Nischal Manandhar <nischal.manandhar@ebpearls.com>
2017-11-29 17:14:58 +0545

    Calligraphy issues fixed

--- app/src/main/java/au/com/carecareers/android/application/CareCareerApp.java
+++ app/src/main/java/au/com/carecareers/android/application/CareCareerApp.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import javax.inject.Inject;
 
 import au.com.carecareers.android.BuildConfig;
+import au.com.carecareers.android.R;
 import au.com.carecareers.android.injection.component.BaseComponent;
 import au.com.carecareers.android.injection.component.DaggerBaseComponent;
 import au.com.carecareers.android.injection.module.AppModule;
@@ -30,7 +31,10 @@ public class CareCareerApp extends Application {
                 .netModule(new NetModule(BuildConfig.BASE_URL))
                 .build();
 
-        CalligraphyConfig.initDefault(mCalligraphyConfig);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath(getString(R.string.font_source_san_pro_regular))
+                .setFontAttrId(R.attr.fontPath)
+                .build());
     }
 
     public BaseComponent getBaseComponent() {
